19ce489a3bd6d5936000a2b08132baacb002a702
aceflowcap <ai_powered_agent@163.com>
2026-04-22 09:46:34 +0000

    finish chapter 00

---
+++ ex_chap00.cpp
@@ -0,0 +1,57 @@
+// Exercises for Chapter 0
+
+#include <iostream>
+
+// ex0-2
+// int main()
+// {
+//     std::cout << "This (\") is a quote, and this (\\) is a backslash." << std::endl;
+//     return 0;
+// }
+
+// ex0-3
+// int main()
+// {
+//     std::cout << "This (\t) is a tab.\nMaybe (\t)sometimes not." << std::endl;
+//     std::cout << "00000012000000" << std::endl;
+//     // a tab (\t) means 2 spaces?
+//     return 0;
+// }
+
+
+// ex0-4
+// int main()
+// {
+//     std::cout << "// a small C++ program" << std::endl;
+//     std::cout << "#include <iostream>" << std::endl;
+//     std::cout << "int main()" << std::endl;
+//     std::cout << "{" << std::endl;
+//     std::cout << "    std::cout << \"Hello, world!\" << std::endl;" << std::endl;
+//     std::cout << "    return 0;" << std::endl;
+//     std::cout << "}" << std::endl;
+// }
+
+// ex0-5~6: valid program?
+// int main()  {{{{std::cout << "Hello, world!" << std::endl;}}}}
+
+
+// ex0-7~8
+// int main()
+// {
+//     // This is a comment
+//     //   that extends several lines using /* and */ as delimeters */
+//     std::cout << "Does this work?" << std::endl;
+// }
+
+
+// ex0-9 shortest program
+// int main() {return 0;}
+
+
+// ex0-10 shortest program
+int main() 
+{
+    std::cout << "Hello," << std::endl;
+    std::cout << "world!" << std::endl;
+    return 0;
+}
